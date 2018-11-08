dceceb3214ca57c9f0f9e9753278f3c3f7c2a7ba
vikks15 <lva9@mail.ru>
2018-11-08 17:01:02 +0300

    Raw game v2

--- public/scripts/game/Game.mjs
+++ public/scripts/game/Game.mjs
@@ -3,22 +3,46 @@ import Map from '/scripts/game/Map.mjs';
 
 export default class Game {
     constructor(mapSize, playersCount) {
-      this.map = new Map(mapSize)
-  
+      this.map = MapBGameViewlder.generateMap(distribution)
       this.currentPlayer = 1;
       this.playersCardId = [0, -1, -2];
       this.hovered = false;
       this.playersCount = playersCount;
+      this.scores = [0, 0, 0];
+      this.timeOut = this.startTimer();
+      this.totalGoldCount = this.map.getTotalGoldCount();
+      console.log(this.totalGoldCount)
   
       for(let i = 1; i <= mapSize*mapSize; ++i) {
-        GameView.setEventListener('click', "gamecard-" + i, flipCard())
+        GameView.setEventListener('click', "gamecard-" + i, flipCard)
       }
   
       for (let i = 1; i <= playersCount; i++) {
         let ship = GameView.getAreaData("ship-" + i);
         GameView.placeDiv("player-" + i, ship[0], ship[1]);
-        GameView.setEventListener('click', 'player-' + i, playerClick())
+        GameView.setEventListener('click', 'player-' + i, playerClick)
       }
+    }
+  
+    checkForWin() {
+      for (let i = 1; i < this.playersCount; i++) {
+        if (this.scores[i] > this.totalGoldCount / 2) {
+          return true;
+        }
+      }
+      return false;
+    }
+  
+    startTimer() {
+      return window.setTimeout(function() {
+        game.currentPlayer++;
+        if (game.currentPlayer % (game.playersCount + 1) == 0) {
+          game.currentPlayer = 1;
+        }
+        game.hovered = false;
+        GameView.resetOpacity();
+        alert("Время вашего хода истекло");
+      }, 10000);
     }
   
     playerClick(id) {
@@ -53,6 +77,8 @@ export default class Game {
         return false;
       }
   
+      window.clearTimeout(this.timeOut);
+  
       for (let i = 1; i < this.playersCount + 1; i++) {
         if (this.playersCardId[i] == cardID) {
           let cardData = GameView.getAreaData("ship-" + i)
@@ -64,17 +90,39 @@ export default class Game {
       let cardData = GameView.getAreaData("gamecard-" + cardID)
       GameView.placeDiv("player-" + this.currentPlayer, cardData[0], cardData[1])
       this.playersCardId[this.currentPlayer] = cardID;
+  
+  
+      let cardType = this.map.getCardType(cardID);
+      let cardObject = CardBGameViewlder.bGameViewld(cardType)
+      cardObject.apply(this);
+  
+      if (this.checkForWin()) {
+        alert("Игрок " + this.currentPlayer + " выиграл! Вы можете продолжать игру.");
+      }
+  
+  
       this.currentPlayer++;
       if (this.currentPlayer % (this.playersCount + 1) == 0) {
         this.currentPlayer = 1;
       }
   
+      switch (cardType) {
+        case CARDTYPES.GOLD:
+          document.getElementById("gamecard-" + cardID).getElementsByTagName('img')[0].src = 'gold.png';
+          break;
+        case CARDTYPES.KILL:
+          document.getElementById("gamecard-" + cardID).getElementsByTagName('img')[0].src = 'kill.png';
+          break;
+        default:
+          document.getElementById("gamecard-" + cardID).getElementsByTagName('img')[0].src = 'water.png';
+          break;
+      }
+  
       GameView.resetOpacity();
   
       this.hovered = false;
+      this.timeOut = this.startTimer();
       return true;
-      // let cardType = map.getCardType(cardID);
-      // let cardObject = CardBGameViewlder.bGameViewld(cardType)
     } 
   }
 
