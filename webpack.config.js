f7b8752c9c9ce78d18b7fb2dd1d25f81dea595b4
Moleque <molecada@yandex.ru>
2018-11-20 15:08:50 +0300

    Webpack test

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,55 @@
 const path = require('path');
 
 module.exports = {
-	entry: {
-		main: [
-			path.resolve(__dirname, 'public') + '/scripts/main.js',
-		],
+	// entry: {
+	// 	main: [
+	// 		path.resolve(__dirname, 'public') + '/scripts/main.js',
+	// 	],
+	// },
+	// module: {
+	// 	rules: [
+	// 		{
+	// 			test : /\.(js|jsx|mjs)$/,
+	// 			exclude: /node_modules/,
+	// 			use: {
+	// 				loader: 'babel-loader',
+	// 				options: {
+	// 					presets: [['@babel/preset-env']]
+	// 				}
+	// 			}
+	// 		},
+	// 	],
+	// },
+
+	entry: './public/scripts/main.js',
+	output: {
+		path: path.resolve(__dirname, 'build'),
+		filename: 'bundle.js',
+		publicPath: '/build/'
 	},
+
 	module: {
 		rules: [
 			{
-				test : /\.(js|jsx|mjs)$/,
-				exclude: /node_modules/,
-				use: {
-					loader: 'babel-loader',
-					options: {
-						presets: [['@babel/preset-env']]
+				test: /\.(js|mjs)$/,
+				use: [
+					{
+						loader: 'babel-loader',
+						options: {presets: ['env']}
 					}
-				}
+				]
 			},
-		],
+			{
+				test: /\.pug$/,
+				use: [
+					{
+						loader: 'pug-loader',
+						options: {pretty: true}
+					}
+				]
+			}
+		]
 	},
+
+	devtool: "source-map"
 };
